1d43ba3e1ccbb465d006c8ae6a4bffee67092216
Robson Locatelli <rlocatelli9@gmail.com>
2023-08-12 09:09:42 -0300

    feat: created migrations [users and meals]

---
+++ src/database/migrations/20230805125501_create-meals.ts
@@ -0,0 +1,22 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('owner').notNullable().index()
+    table.uuid('id').primary()
+    table.text('title').notNullable()
+    table.text('description').nullable()
+    table.text('type').notNullable()
+    table.datetime('datetime').notNullable()
+    table.boolean('in_diet').defaultTo(false).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+    table.timestamp('deleted_at').nullable()
+
+    table.foreign('owner').references('user_id').inTable('users')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
