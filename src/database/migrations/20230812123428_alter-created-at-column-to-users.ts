65cf426e5570ceade1a41102e44f1cd81749d0dc
Robson Locatelli <rlocatelli9@gmail.com>
2023-08-12 09:49:25 -0300

    feat: created migrations to alter table users

---
+++ src/database/migrations/20230812123428_alter-created-at-column-to-users.ts
@@ -0,0 +1,19 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.dropColumn('created_at')
+  })
+  await knex.schema.alterTable('users', (table) => {
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.dropColumn('created_at')
+  })
+  await knex.schema.alterTable('users', (table) => {
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
